697b7b8f5ecff62cb7290cc353b5563405900e41
Mike Ford <michaelford2@mail.weber.edu>
2025-09-10 18:15:49 -0600

    added functionality to buttons and made custom settings in buttons.js file

--- scripts/button.js
+++ scripts/button.js
@@ -1,4 +1,19 @@
 function CipherSageFave() {
+    document.getElementById('Setting').innerHTML="Cipher Sage Fave"; // this changes the condition text from "" to "Cipher Sage Fave" after clicking the button
     document.getElementById('mainPage').style="background-color: black; color: green;";
+    document.getElementById('aboutme').style="background-color: white; color: black;";
 }
+
+function OblivionSpark() {
+    document.getElementById('Setting').innerHTML="Oblivion Spark"; // this changes the condition text from "" to "Oblivion Spark" after clicking the button
+    document.getElementById('CS1').style="./images/Digital_Mike.png;";
+    document.getElementById('mainPage').style="background-color: #2d27caec;";
+    document.getElementById('home').style="background-color: #2d27caec;";
+}
+
+function Loveless() {
+    document.getElementById('mainPage').style="background-color: pink; color: white;";
+    document.getElementById('Setting').innerHTML="Loveless"; // this changes the condition text from "" to "Loveless" after clicking the button
+}
+
 
