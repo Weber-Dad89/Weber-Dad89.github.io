8120aac7d2b4e1ecd9bc987119ceadbabb961c11
Mike Ford <michaelford2@mail.weber.edu>
2025-09-15 21:51:58 -0600

    added calculator.html page

--- scripts/button.js
+++ scripts/button.js
@@ -1,19 +1,27 @@
 function CipherSageFave() {
     document.getElementById('Setting').innerHTML="Cipher Sage Fave"; // this changes the condition text from "" to "Cipher Sage Fave" after clicking the button
     document.getElementById('mainPage').style="background-color: black; color: green;";
+    document.getElementById('homeNav').style.padding = "10px";
     document.getElementById('aboutme').style="background-color: white; color: black;";
 }
 
 function OblivionSpark() {
     document.getElementById('Setting').innerHTML="Oblivion Spark"; // this changes the condition text from "" to "Oblivion Spark" after clicking the button
-    document.getElementById('CS1').style="./images/Digital_Mike.png;";
     document.getElementById('mainPage').style="background-color: #2d27caec;";
+    document.getElementById('CS1').style="./images/Digital_Mike.png;";
     document.getElementById('home').style="background-color: #2d27caec;";
 }
 
 function Loveless() {
-    document.getElementById('mainPage').style="background-color: pink; color: white;";
     document.getElementById('Setting').innerHTML="Loveless"; // this changes the condition text from "" to "Loveless" after clicking the button
+    document.getElementById('mainPage').style="background-color: pink; color: white;";
+    
+}
+
+function This() {
+    document.getElementById('demobtn').innterHTML="Ç$"; // this changes the button text from "" to Ç$ my signature
+    document.getElementById('demo').innterHTML="Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consect"
+    "etur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque."
 }
 
 
