32b500194a0e5bd7c5ff3c8c01407948715ebf8d
Mike Ford <michaelford2@mail.weber.edu>
2025-11-10 01:32:40 -0700

    Edited assignmentNotes - completed addItem functionality| - completed removeItem functionality - theme settings persist on page w/ light and dark modes - several cookies made - task lists displays on page
    TODO:
    - ADD taskCounter
    - Get tasklist to persist after signout and switching users.
    - Implement Fetch API in TodoPage.js
    - Add link to and from homepage

--- scripts/TodoPage.js
+++ scripts/TodoPage.js
@@ -1,1 +1,14 @@
 let data= null;
+
+
+       // GET /quotes/random
+        // const quoteURL= "https://api.api-ninjas.com/v2/quotes";
+
+        // async function fetchData() {
+        //     const response= await fetch(quoteURL);
+        //     const data= await response.json();
+        //     console.log(data);
+        // }
+        // let quote= fetchData();
+        // document.getElementById("quoteAPI").innerText= quote;
+        // console.log(quote);
