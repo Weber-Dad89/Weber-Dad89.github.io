c7ec2416bfacede27c1ea9b7f02c54d70a00b8af
Mike Ford <michaelford2@mail.weber.edu>
2025-11-10 14:50:23 -0700

    Finished Fetch API implementation TODO: fix promise error Finished Todo Page Added randomInfo page to save code for later. Added element for quoteContent

--- scripts/TodoPage.js
+++ scripts/TodoPage.js
@@ -1,17 +1,50 @@
-let data= null;
+//let data= null;
 
-function quoteDisplayer(something) {
-       document.getElementById("").innerHTML= something;
+// API Source: "https://api-ninjas.com/api"
+let url = 'https://api.api-ninjas.com/v2/randomquotes?categories=success,wisdom';
+let options = {
+       method: 'GET',
+       headers: { 'X-api-key': 'E1J+4lJXFweTqLagqU6h+Q==pFY5veVVYAMSRHUt' }
 }
 
-       // GET /quotes/random
-        // const quoteURL= "https://api.api-ninjas.com/v2/quotes";
+       
 
-        // async function fetchData() {
-        //     const response= await fetch(quoteURL);
-        //     const data= await response.json();
-        //     console.log(data);
-        // }
-        // let quote= fetchData();
-        // document.getElementById("quoteAPI").innerText= quote;
-        // console.log(quote);
+async function quoteDisplayer() {
+       try {
+              const response= await fetch(url, options);
+              //let response= await fetch("https://api.api-ninjas.com/v2/randomquotes?categories=success,wisdom", options);
+              let data= await response.json();
+              // .then(res => res.json());
+              // .then(data => console.log(data));
+              if (!response.ok) {
+                     throw new Error(`HTTP error! status: ${response.status}`);
+              }
+              console.log(data);
+              console.log("quote: ", data[0].quote);
+              console.log("author ", data[0].author);
+              let randomQuote= data[0].quote;
+              let quoteAuthor= data[0].author;
+              document.getElementById("randomQuote").innerText= "Quote: " + randomQuote;
+              document.getElementById("quoteAuthor").innerText= "Author: " + quoteAuthor;
+       } catch (error) {
+              console.error("Error fetching quote: ", error);
+       }
+}
+
+// window.onload= function() {
+//  let promise= new Promise(function(resolve, reject) {
+//        //let dataSuccessful= quoteDisplayer();
+//        if (dataSuccessful)
+//               resolve(data);
+//        else
+//               reject("Unable to load quote");
+//        });
+//        promise.then(
+//               function(normalValue) {quoteDisplayer(normalValue); },
+//               function(errorValue) {quoteDisplayer(errorValue);}
+//        ).finally(function() {
+//               console.log("Cleaning up...")
+//        });
+// };
+
+quoteDisplayer();
