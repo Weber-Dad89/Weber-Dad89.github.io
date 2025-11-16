75b0097751ead2e38639e1366d6a1fcc8466322b
Michael Ford <michaelford2@mail.weber.edu>
2025-11-15 19:29:13 -0700

    added instances for class objects and edited notes TODO: add image thumbnails

---
+++ scripts/mediaCollection.js
@@ -0,0 +1,19 @@
+// Game class with attributes
+class Games {
+    constructor(name, genre, platform) {
+    // Properties that each object should have (properties are another term for key-value pairs in an object)
+    this.name= name;
+    this.genre= genre
+    this.platform= platform;
+    this.image= this.image;
+    };
+    //  Methods: the functions the object has access to in order to do tasks
+    // 
+}
+
+// array of objects containing media items
+games= [
+    {"name": "Diablo 4", "genre": "action", "platform": "Xbox"},
+    {"name": "Demon Souls", "genre": "RPG", "platform": "PS5"},
+    {"name": "Mario Kart World", "genre": "racing", "platform": "Nintendo Switch 2"},
+]
